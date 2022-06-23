93bdd641c3292d4c2c098e63db33a67fd9313db2
IlzeRieba <78449581+IlzeRieba@users.noreply.github.com>
2022-06-23 16:20:26 +0200

    imported Weather Search application and css file

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,65 @@
 import React from "react";
-import axios from "axios";
+import "./Weather.css";
 
-export default function Weather()
-{
-    function handleResponse(response) {
-        return (`The weather in London is ${response.data.main.temp} C `)
-    }
-    let apiKey = "9860ec3dfdc6dad49e4f6a4e8c42cfa9";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+export default function Weather() {
+  let weatherData = {
+    city: "Ericeira",
+    date: "Sunday 29th May 11:08",
+    temperature: "21",
+    description: "cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    humidity: "30%",
+    windSpeed: "10 m/s"
+  };
 
-axios.get(apiUrl).then(handleResponse);
+  return (
+    <div className="App">
+      <form>
+        <input
+          class="search-box"
+          type="text"
+          placeholder="Enter a city"
+          autocomplete="off"
+        />
+        <input class="search-button" type="submit" value="Search" />
+      </form>
 
-return (
-        <h1>
-            Hello Landes! Time to go surfing!
-        </h1>
-    );
+      <div className="row">
+        <div className="col-6">
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li className="current-date-time">
+              Last updated {weatherData.date}
+            </li>
+            <li className="current-temperature">
+              {" "}
+              <strong>{weatherData.temperature}℃ </strong>
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+            </li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <ul className="current-conditions">
+            <li> Wind speed: {weatherData.windSpeed}</li>
+            <li> Humidity: {weatherData.humidity} </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="spacing"></div>
+
+      <div class="footnote">
+        <small>
+          <a href="https://github.com/IlzeRieba/shecodes_vanilla_weather_app.git">
+            Open source code
+          </a>{" "}
+          by Ilze Rieba
+        </small>
+      </div>
+    </div>
+  );
 }
